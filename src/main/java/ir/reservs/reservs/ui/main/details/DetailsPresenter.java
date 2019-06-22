3d71bbb174ae73979dca278c4a6ddfd4e4b499f1
ZarinPal@Travis <ZarinPal@Travis>
2019-06-22 23:19:17 +0430

    add details package

---
+++ src/main/java/ir/reservs/reservs/ui/main/details/DetailsPresenter.java
@@ -0,0 +1,13 @@
+package ir.reservs.reservs.ui.main.details;
+
+public class DetailsPresenter implements DetailsContract.Presenter {
+    @Override
+    public void onAttach(DetailsContract.View view) {
+
+    }
+
+    @Override
+    public void onDetach() {
+
+    }
+}
